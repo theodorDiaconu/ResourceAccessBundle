9abf44f9a59f41566e566bf2ed480ec2f7a74b6f
beliveyourdream <beliveyourdream@gmail.com>
2013-06-13 15:51:42 +0300

    added license

--- Model/RoleHierarchy.php
+++ Model/RoleHierarchy.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the ResourceAccessBundle.
+ *
+ * (c) Theodor Diaconu <diaconu.theodor@gmail.com>
+ * (c) Alexandru Miron <beliveyourdream@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AT\ResourceAccessBundle\Model;
 
 use AT\ResourceAccessBundle\Util\RoleNode;
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,13 @@
 <?php
 
-/**
- * @author Theodor Diaconu <diaconu.theodor@gmail.com>
- * @author Alexandru Miron <beliveyourdream@gmail.com>
+/*
+ * This file is part of the ResourceAccessBundle.
+ *
+ * (c) Theodor Diaconu <diaconu.theodor@gmail.com>
+ * (c) Alexandru Miron <beliveyourdream@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
